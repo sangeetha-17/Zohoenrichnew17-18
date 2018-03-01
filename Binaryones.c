1b7c41a109d0605a970ac8cff695cbfa700f08b2
sangeetha-17 <35231194+sangeetha-17@users.noreply.github.com>
2018-03-01 19:17:26 +0530

    Update Binaryones.c

--- Binaryones.c
+++ Binaryones.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
-void main(){
-int n,r,count=0;
-scanf ("%d",&n);
-while(n>0){
-r=n%2;
-if(r==1)
-count++;
-n=n/2;
-}
-printf("%d",count);
+void main()
+{
+  int n,r,count=0;
+  scanf ("%d",&n);
+  while(n>0)
+  {
+    r=n%2;
+    if(r==1)
+    count++;
+    n=n/2;
+  }
+  printf("%d",count);
 }
